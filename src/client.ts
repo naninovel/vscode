0cda978685d70eeac21252b0594e850a986325ec
Artyom Sovetnikov <elringus@gmail.com>
2021-09-17 13:21:24 +0300

    remove telemetry

--- src/client.ts
+++ src/client.ts
@@ -3,12 +3,6 @@ import * as lsp from "vscode-languageclient/node";
 import * as path from "path";
 import { existsSync } from "fs";
 import { getLogger } from "./logger";
-import {
-  callWithTelemetryAndErrorHandlingSync,
-  IActionContext,
-  parseError,
-} from "vscode-azureextensionui";
-import { ErrorAction, Message, CloseAction } from "vscode-languageclient/node";
 
 const languageId = "naniscript";
 const dotnetRuntimeVersion = "3.1";
@@ -17,21 +11,15 @@ const packagedServerPath = "server/LanguageServer.dll";
 
 export async function launchLanguageServiceWithProgressReport(
   context: vscode.ExtensionContext,
-  outputChannel: vscode.OutputChannel
-): Promise<void> {
-  await vscode.window.withProgress(
-    {
-      title: "Launching Naninovel language service...",
-      location: vscode.ProgressLocation.Notification,
-    },
-    async () => await launchLanguageService(context, outputChannel)
-  );
+  outputChannel: vscode.OutputChannel): Promise<void> {
+  await vscode.window.withProgress({
+    title: "Launching Naninovel language service...",
+    location: vscode.ProgressLocation.Notification,
+  }, async () => await launchLanguageService(context, outputChannel));
 }
 
-async function launchLanguageService(
-  context: vscode.ExtensionContext,
-  outputChannel: vscode.OutputChannel
-): Promise<void> {
+async function launchLanguageService(context: vscode.ExtensionContext,
+  outputChannel: vscode.OutputChannel): Promise<void> {
   getLogger().info("Launching Naninovel language service...");
 
   const dotnetCommandPath = await ensureDotnetRuntimeInstalled();
@@ -66,96 +54,22 @@ async function launchLanguageService(
     clientOptions
   );
 
-  configureTelemetry(client);
-
   context.subscriptions.push(client.start());
-
   getLogger().info("Naninovel language service started.");
-
   await client.onReady();
-
   getLogger().info("Naninovel language service ready.");
 }
 
 async function ensureDotnetRuntimeInstalled(): Promise<string> {
   const result = await vscode.commands.executeCommand<{ dotnetPath: string }>(
-    "dotnet.acquire",
-    {
-      version: dotnetRuntimeVersion,
-      requestingExtensionId: extensionId
-    }
-  );
-
-  if (!result) {
-    throw new Error(`Failed to install .NET runtime v${dotnetRuntimeVersion}.`);
-  }
-
+    "dotnet.acquire", { version: dotnetRuntimeVersion, requestingExtensionId: extensionId });
+  if (!result) throw new Error(`Failed to install .NET runtime v${dotnetRuntimeVersion}.`);
   return path.resolve(result.dotnetPath);
 }
 
 function ensureLanguageServerExists(context: vscode.ExtensionContext): string {
   const languageServerPath = context.asAbsolutePath(packagedServerPath);
-
-  if (!existsSync(languageServerPath)) {
-    throw new Error(
-      `Language server does not exist at '${languageServerPath}'.`
-    );
-  }
-
+  if (!existsSync(languageServerPath))
+    throw new Error(`Language server does not exist at '${languageServerPath}'.`);
   return path.resolve(languageServerPath);
-}
-
-function configureTelemetry(client: lsp.LanguageClient) {
-  const startTime = Date.now();
-  const defaultErrorHandler = client.createDefaultErrorHandler();
-
-  client.onTelemetry(
-    (telemetryData: {
-      eventName: string;
-      properties: { [key: string]: string | undefined };
-    }) => {
-      callWithTelemetryAndErrorHandlingSync(
-        telemetryData.eventName,
-        (telemetryActionContext) => {
-          telemetryActionContext.errorHandling.suppressDisplay = true;
-          telemetryActionContext.telemetry.properties =
-            telemetryData.properties;
-        }
-      );
-    }
-  );
-
-  client.clientOptions.errorHandler = {
-    error(
-      error: Error,
-      message: Message | undefined,
-      count: number | undefined
-    ): ErrorAction {
-      callWithTelemetryAndErrorHandlingSync(
-        "naniscript.lsp-error",
-        (context: IActionContext) => {
-          context.telemetry.properties.jsonrpcMessage = message
-            ? message.jsonrpc
-            : "";
-          context.telemetry.measurements.secondsSinceStart =
-            (Date.now() - startTime) / 1000;
-
-          throw new Error(`Error: ${parseError(error).message}`);
-        }
-      );
-      return defaultErrorHandler.error(error, message, count);
-    },
-    closed(): CloseAction {
-      callWithTelemetryAndErrorHandlingSync(
-        "naniscript.lsp-error",
-        (context: IActionContext) => {
-          context.telemetry.measurements.secondsSinceStart =
-            (Date.now() - startTime) / 1000;
-
-          throw new Error(`Connection closed`);
-        }
-      );
-      return defaultErrorHandler.closed();
-    },
-  };
 }
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,12 @@
 import * as vscode from "vscode";
-
 import { createLogger } from "./logger";
 import { launchLanguageServiceWithProgressReport } from "./client";
-import { activateWithTelemetryAndErrorHandling } from "./telemetry";
 import { createAzExtOutputChannel } from "vscode-azureextensionui";
 
 export async function activate(context: vscode.ExtensionContext): Promise<void> {
   const outputChannel = createAzExtOutputChannel("NaniScript", "naniscript");
-  await activateWithTelemetryAndErrorHandling(context, outputChannel,
-    async () => {
-      createLogger(context, outputChannel);
-      await launchLanguageServiceWithProgressReport(context, outputChannel);
-    });
+  createLogger(context, outputChannel);
+  await launchLanguageServiceWithProgressReport(context, outputChannel);
 }
 
 export function deactivate(): void { }
