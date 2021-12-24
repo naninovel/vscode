913c4e291d5ca53f93f6a42d150c74183c59ef3b
Artyom Sovetnikov <elringus@gmail.com>
2021-12-24 23:07:12 +0300

    iteration

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,6 @@
 import { window, ProgressLocation, Uri, ExtensionContext, OutputChannel } from "vscode";
 import { bootLanguage } from "./language";
 import { bootBridging } from "./bridging";
-import { onWorkerPong } from "naninovel-editor";
 
 export async function activate(context: ExtensionContext) {
     await window.withProgress({
@@ -24,6 +23,5 @@ function bootWorker(context: ExtensionContext, channel: OutputChannel): Worker {
     const worker = new Worker(uri.toString());
     worker.onerror = e => channel.appendLine(`Worker error: ${e.message}`);
     worker.onmessageerror = e => channel.appendLine(`Worker message error: ${e}`);
-    onWorkerPong(worker, m => channel.appendLine(m));
     return worker;
 }
