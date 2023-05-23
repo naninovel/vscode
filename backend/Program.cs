c4cbe42e7e5098abecd47af397ce6bdaa2aa354c
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-05-23 18:39:39 +0300

    iteration

--- backend/Program.cs
+++ backend/Program.cs
@@ -8,8 +8,6 @@ namespace Naninovel.Language.VSCode;
 
 public static class Program
 {
-    static Program () => ConfigureJson();
-
     public static void Main ()
     {
         JSLogger.LogInfo($"Naninovel language version: {typeof(Language).Assembly.GetName().Version}");
