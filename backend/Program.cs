94f17e42b3efbc729025ff7bcf965f4e725592ca
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-06-22 16:20:06 +0300

    fix symbol request

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,5 @@
-﻿using DotNetJS;
+﻿using System.Text.Json.Serialization;
+using DotNetJS;
 using Naninovel.Bindings;
 using static Naninovel.Bindings.Utilities;
 
@@ -8,6 +9,13 @@ namespace Naninovel.Language.VSCode;
 
 public static class Program
 {
+    static Program ()
+    {
+        JS.Runtime.ConfigureJson(options =>
+            options.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+        );
+    }
+
     public static void Main ()
     {
         JSLogger.LogInfo($"Naninovel language version: {typeof(Language).Assembly.GetName().Version}");
