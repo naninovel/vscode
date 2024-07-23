8bbb793a6144f878ee96feb36001428c254e0992
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2024-07-23 23:14:41 +0300

    etc

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,4 +1,4 @@
-sed -r "s/\"version\": \"(.+\.)0\",/\"version\": \"\1$(date +%Y%m%d)\",/" -i "package.json"
+sed -r "s/\"version\": \"(.+\.)0\",/\"version\": \"\1$(date +%y%m%d)\",/" -i "package.json"
 npm run build
 npm run package
 sed -r "s/\"version\": \"(.+\.).+\",/\"version\": \"\10\",/" -i "package.json"
